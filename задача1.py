ab039b8a118c9e862c11f4f15e21f78972c402e4
ilonmaskisnota <153349058+ilonmaskisnota@users.noreply.github.com>
2024-04-21 23:49:16 +1000

    Add files via upload

---
+++ задача1.py
@@ -0,0 +1,7 @@
+number = [2,4,1,6,3,]
+list = min(number)
+number.remove(list)
+print("Наименьшее число:", list)
+nim1 = min(number)
+number.remove(nim1)
+print("наименьшее наименьшего:", nim1)
---
+++ задача2.py
@@ -0,0 +1,4 @@
+list = [-5,4,3,9,2,7,-10]
+
+for i in range(0,len(list),-1):
+    print(list[i], list[i+1], i)
